6766bca95be3d3787867f72e3abb435c406d45eb
Tecnowild <urielbarriosh@gmail.com>
2026-06-27 14:22:17 -0500

    Cambio Original

--- _frontEnd/src/features/catalog/components/CatalogSidebar.tsx
+++ _frontEnd/src/features/catalog/components/CatalogSidebar.tsx
@@ -93,7 +93,7 @@ export function CatalogSidebar({
           className="text-lg font-black text-[#2a1115]"
           style={{ fontFamily: "'Outfit', sans-serif" }}
         >
-          Corrección, aquí estaba filtrar
+          Filtrar
         </h2>
         <button
           onClick={resetFilters}
